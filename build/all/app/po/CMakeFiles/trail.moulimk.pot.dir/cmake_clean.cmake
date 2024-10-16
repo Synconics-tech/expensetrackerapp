6b1b0a8d5d76d64cdbd9980aee1521dd5e97a31a
mouligangadhar <mouligangadharpedireddy@gmail.com>
2024-10-16 16:19:00 +0530

    first commit

---
+++ build/all/app/po/CMakeFiles/trail.moulimk.pot.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/trail.moulimk.pot"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/trail.moulimk.pot.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
